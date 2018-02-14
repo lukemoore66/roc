7bd79126a6656f2b5c651a7aaa89285815296d2c
lukemoore66 <33060484+lukemoore66@users.noreply.github.com>
2018-02-14 21:55:28 +0800

    Add files via upload

--- libroc.ps1
+++ libroc.ps1
@@ -12,8 +12,8 @@ Options:
 	[-InputPath] <String>]	- A Valid File Or Folder Path For Input File(s).
 	[-Crf <Int>]		- An Integer Ranging From 0-51. (AKA: Video Quality 14-25 Are Sane Values)
 	[-Preset <String>]	- x264 Preset. placebo, slowest, slower, slow, medium, fast, faster, ultrafast
-	[-OutputPath <String>]	- A Valid File Or Folder Path For Output File(s).
-	[-TempPath <String>]	- A Valid File Or Folder Path For Temporary File(s).
+	[-OutputPath <String>]	- A Valid Folder Path For Output File(s).
+	[-TempPath <String>]	- A Valid Folder Path For Temporary File(s).
 	[-Copy <Switch>]	- Copies (Remuxes) Segments. Very Fast, But May Cause Playback Problems.
 	[-Help <Switch>]	- Shows This Help Menu.
 	
